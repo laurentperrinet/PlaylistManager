4788491c6a4aad37fe2be5fe55b924ef13cd4206
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-09-30 15:21:12 +0200

    First test

---
+++ first_test.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+import youtube_dl
+import argparse
+
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--links_path", type=str, default='links',
+                    help="Chemin vers la liste des liens")
+opt = parser.parse_args()
+print(opt)
+
+# Read links list
+# Lecture des seeds
+with open(opt.links_path, "r") as file:
+    links = file.read().splitlines()
+    
+    print(links)
+
+
+ydl_opts = {
+    'format': 'bestaudio',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(links) 
