c1402ef0ca71ab36e0d0cb0ca664c6bbc62092f3
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-01-18 14:29:48 +0100

    using json as data source

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PlaylistManager
+
+A simple scrapper for sharing audio on the interweb.
+
+Add your bookmarks to the `links.json` file!
--- download.py
+++ download.py
@@ -1,37 +1,40 @@
-from __future__ import unicode_literals
 import youtube_dl
 import argparse
 
 # Parser
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--links_path", type=str, default='links',
-                    help="Chemin vers la liste des liens")
-parser.add_argument("-f", "--format", type=str, default='mp3',
+parser.add_argument("-p", "--links_path", type=str, default='links.json',
+                    help="Chemin vers la liste des liens en format JSON")
+parser.add_argument("-f", "--format", type=str, default='opus',
                     help="Type de format")
 parser.add_argument("-q", "--quality", type=str, default='192',
                     help="Niveau de qualité")
+parser.add_argument('-verbose', type=bool, default=True,
+                    help="Verbosité")
+
 opt = parser.parse_args()
-print(opt)
+if opt.verbose: print(opt)
 
-# Read links list
-# Lecture des seeds
+# Lecture des liens
+import json
 with open(opt.links_path, "r") as file:
-    links = file.read().splitlines()
-    
-    print(links)
+    links = json.load(file)
+    if opt.verbose: print(links)
 
+for link in links.keys():
+    if opt.verbose: print(link, links[link])
 
-ydl_opts = {
-    'format': 'bestaudio',
-    'outtmpl': '%(title)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': opt.format,
-        'preferredquality': opt.quality,
-    }],
-}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(links) 
-    info = ydl.extract_info(links[0], download=False)
-    print(info.get('title', None))
-    
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'output/{link}/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': opt.format,
+            'preferredquality': opt.quality,
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        if opt.verbose:
+            info = ydl.extract_info(links[link], download=False)
+            print(info.get('title', None))
+        ydl.download([links[link]])
